64db333ac9b9209e2f9efd694ff64a2923333d74
wonjunYun <windy3257@naver.com>
2022-05-17 18:17:52 +0900

    nweet

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "./styles.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
